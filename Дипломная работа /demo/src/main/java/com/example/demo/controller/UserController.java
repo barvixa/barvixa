b31553e188e27867d442e3b550b8e4c0311db591
i22s0649 <i22s0649@students.it-college.ru>
2025-11-14 21:45:30 +0500

    123

--- Дипломная работа /demo/src/main/java/com/example/demo/controller/UserController.java
+++ Дипломная работа /demo/src/main/java/com/example/demo/controller/UserController.java
@@ -10,38 +10,40 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/create")
     public CreateUserResponse createUser(@RequestBody CreateUserResponse request) {
         return userService.createUser(request);
         
     }
+
        @GetMapping("/all")
     public List<User> getUsers() {
         return userService.findAll();
     }
+
     @PutMapping("/update/{id}")
-public CreateUserResponse updateUser(
+    public CreateUserResponse updateUser(
     @PathVariable Long id,  // Изменил на Long
     @RequestBody CreateUserResponse request) {
-    return userService.updateUserWithQuery(id, request);
-}
-@DeleteMapping("/delete/{id}")
-public CreateUserResponse deleteUser(@PathVariable Long id) {
-    return userService.deleteUser(id);
-}
-@GetMapping("/bonuses/total")
-public ResponseEntity<BigDecimal> getTotalBonuses() {
+        return userService.updateUserWithQuery(id, request);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CreateUserResponse deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @GetMapping("/bonuses/total")
+    public ResponseEntity<BigDecimal> getTotalBonuses() {
     BigDecimal total = userService.getTotalBonusBalance();
-    return ResponseEntity.ok(total);
-}
+        return ResponseEntity.ok(total);
+    }
 }
